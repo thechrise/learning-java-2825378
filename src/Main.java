d4eb3bbd8a0ddeb6c1ced4d7d25186783247a105
Chris <cevans@outreach.com>
2021-12-23 12:29:59 -0700

    Complete episode

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
 public class Main {
 
-    public static void calculateTotalMealPrice(double listedMealPrice,
+    public static double calculateTotalMealPrice(double listedMealPrice,
                                                double tipRate,
                                                double taxRate) {
         double tip = tipRate * listedMealPrice;
         double tax = taxRate * listedMealPrice;
-        double result = listedMealPrice + tip + tax;
-        System.out.println("Your total meal price is " + result);
+        return listedMealPrice + tip + tax;
     }
 
     public static void main(String[] args) {
-        
+        double groupTotalMealCost = calculateTotalMealPrice(100,.2,.08);
+        System.out.println(groupTotalMealCost);
+
+        double individualMealPrice = groupTotalMealCost / 5;
+        System.out.println(individualMealPrice);
     }
 
 }
