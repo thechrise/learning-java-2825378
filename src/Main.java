4f67031a1a5aa66a2d8ba388b3aec700297987ef
Chris <cevans@outreach.com>
2021-12-23 12:58:22 -0700

    Complete episode

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        Triangle triangleA = new Triangle(15,8,15,8, 17);
+        Triangle triangleB = new Triangle(3, 2.597,3,3,3);
     }
 
 }
