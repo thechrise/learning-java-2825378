3d986c25f34adacb023d43766fb2ab8ad7215866
Chris <cevans@outreach.com>
2021-12-23 12:41:11 -0700

    Complete episode

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        double salary = calculateGrossYearlySalary(40, 50, 7);
+        System.out.println(salary);
     }
 
+    public static double calculateGrossYearlySalary(double weeklyHours,
+                                                    double hourlyWage,
+                                                    int vacationDays){
+        if (weeklyHours < 0){
+            return -1;
+        }
+        if (hourlyWage < 0){
+            return -1;
+        }
+        return weeklyHours * hourlyWage * 52 - (vacationDays * 8 * hourlyWage);
+    }
 }
