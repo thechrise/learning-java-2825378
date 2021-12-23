c1b8fbb0806577436284fe23d03256503eb624b4
Chris <cevans@outreach.com>
2021-12-23 12:15:56 -0700

    Complete episode

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void announceDeveloperTeaTime() {
+        System.out.println("Waiting for developer tea time...");
+        System.out.println("Type in a random word and press Enter to start developer tea time.");
 
+        Scanner input = new Scanner(System.in);
+        input.next();
+        System.out.println("It's developer tea time!");
     }
 
+    public static void main(String[] args) {
+        announceDeveloperTeaTime();
+    }
 }
