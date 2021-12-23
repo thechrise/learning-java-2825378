b33c60f60e573d418d08b73814669fcae4a3fe5c
Chris <cevans@outreach.com>
2021-12-23 11:55:27 -0700

    Complete episode

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,35 @@
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String args[]) {
-        String question = "";
-        String choiceOne = "";
-        String choiceTwo = "";
-        String choiceThree = "";
-
-        String correctAnswer = choiceTwo;
+    public static void main(String[] args) {
+        String question = "What's Chris' favorite candy?";
+        String choiceOne = "Snickers";
+        String choiceTwo = "Skittles";
+        String choiceThree = "Tootsie Rolls";
 
         // Write a print statement asking the question
+        System.out.println(question);
         // Write a print statement giving the answer choices
+        System.out.println("1) " + choiceOne);
+        System.out.println("2) " + choiceTwo);
+        System.out.println("3) " + choiceThree);
 
         // Have the user input an answer
+        Scanner scanner = new Scanner(System.in);
         // Retrieve the user's input
+        int input = scanner.nextInt();
 
         // If the user's input matches the correctAnswer...
-        // then the user is correct and we want to print out a congrats message to the user.
+        if (input == 2){
+            System.out.println("Congrats! That's correct!");
+        } else {
+            System.out.println("So, sorry. The correct answer was #2 - " + choiceTwo);
+        }
+        // then the user is correct, and we want to print out a congrats message to the user.
 
         // If the user's input does not match the correctAnswer...
-        // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
+        // then the user is incorrect, and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
 
     }
 
